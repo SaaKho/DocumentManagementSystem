8ea45938524344838d9efff5d872783b51eb1734
Malik Saadan Khokhar <109228868+SaaKho@users.noreply.github.com>
2024-09-02 13:33:03 +0500

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import express from "express";
-import authRoutes from "./routes/authRoutes";
-import documentRoutes from "./routes/documentRoutes";
+import authRoutes from "./routes/users/authRoutes";
+import documentRoutes from "./routes/document/documentRoutes";
+import tagRoutes from "./routes/document/tagRoutes";
+import downloadRoutes from "./routes/document/downloadRoutes";
+import searchRoute from "./routes/document/searchRoute";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +14,9 @@ app.use(express.json());
 // Routes
 app.use("/api/users", authRoutes);
 app.use("/api/documents", documentRoutes);
+app.use("/api/tags", tagRoutes);
+app.use("/api/download", downloadRoutes);
+app.use("/api/search", searchRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
