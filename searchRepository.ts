7e105344b753f7c967b9f37a01c404a3a01311ad
Malik Saadan Khokhar <109228868+SaaKho@users.noreply.github.com>
2024-10-05 15:27:02 +0500

    Add files via upload

---
+++ searchRepository.ts
@@ -0,0 +1,30 @@
+// src/repository/implementations/SearchRepository.ts
+import { db, documents } from "../../drizzle/schema";
+import { ilike, arrayContains } from "drizzle-orm";
+import { ISearchRepository } from "../interfaces/Isearch.repository";
+import { SearchResultDTO } from "../interfaces/search.dto";
+
+export class SearchRepository implements ISearchRepository {
+  async advancedSearch(
+    tags?: string[],
+    fileName?: string,
+    contentType?: string
+  ): Promise<SearchResultDTO[]> {
+    let query: any = db.select().from(documents);
+
+    if (tags && tags.length > 0) {
+      query = query.where(arrayContains(documents.tags as any, tags as any));
+    }
+
+    if (fileName) {
+      query = query.where(ilike(documents.fileName, `%${fileName}%`));
+    }
+
+    if (contentType) {
+      query = query.where(ilike(documents.contentType, `%${contentType}%`));
+    }
+
+    const results = await query.execute();
+    return results;
+  }
+}
