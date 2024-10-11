c4b0b74ddca20ce785f42919ce409f476c01f00e
Malik Saadan Khokhar <109228868+SaaKho@users.noreply.github.com>
2024-10-11 18:14:50 +0500

    Add files via upload

--- document.dto.ts
+++ document.dto.ts
@@ -1,42 +1,27 @@
-// src/repository/interfaces/document.dto.ts
-
-// DTO for uploading a document
-export interface UploadDocumentDTO {
-  fileName: string;
-  fileExtension: string;
-  contentType: string;
-  tags: string[] | null;
-}
-
-// DTO for creating or uploading a document
-export interface CreateDocumentDTO {
+// src/application/dtos/DocumentDTO.ts
+export interface DocumentDTO {
   id: string;
   fileName: string;
   fileExtension: string;
   contentType: string;
-  tags: string[] | null;
-  createdAt: Date | null;
-  updatedAt: Date | null;
+  tags: string[];
+  createdAt: Date;
+  updatedAt: Date;
+  filePath: string;
 }
 
-// DTO for updating a document
+export interface CreateDocumentDTO {
+  userId: string;
+  fileName: string;
+  fileExtension: string;
+  contentType: string;
+  tags?: string[];
+}
+
 export interface UpdateDocumentDTO {
   fileName?: string;
   fileExtension?: string;
   contentType?: string;
-  tags?: string[] | null;
-  createdAt?: Date | null;  
-  updatedAt?: Date | null; 
-}
-
-
-// // DTO for the document object returned from the database
-export interface GetDocumentDTO {
-  id: string;
-  fileName: string;
-  fileExtension: string;
-  contentType: string;
-  tags: string[] | null;
-  createdAt: Date | null;
-  updatedAt: Date | null;
+  tags?: string[];
+  filePath?: string;
 }
