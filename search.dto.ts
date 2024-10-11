dd5cd98d96a883d6371fc72e32cac2dec62a2aed
Malik Saadan Khokhar <109228868+SaaKho@users.noreply.github.com>
2024-10-11 18:15:36 +0500

    Add files via upload

--- search.dto.ts
+++ search.dto.ts
@@ -1,19 +1,7 @@
-// src/repository/interfaces/search.dto.ts
+// src/application/dtos/SearchResultDTO.ts
+import { DocumentDTO } from "./document.dto";
 
-// DTO for search queries
-export interface AdvancedSearchDTO {
-  tags?: string;
-  fileName?: string;
-  contentType?: string;
-}
-
-// DTO for document search results
 export interface SearchResultDTO {
-  id: string;
-  fileName: string;
-  fileExtension: string;
-  contentType: string;
-  tags: string[];
-  createdAt: Date | null;
-  updatedAt: Date | null;
+  results: DocumentDTO[];
+  totalResults: number;
 }
