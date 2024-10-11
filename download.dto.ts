3f75517f5bd20e327af97743912555765c62fcc9
Malik Saadan Khokhar <109228868+SaaKho@users.noreply.github.com>
2024-10-11 18:15:02 +0500

    Add files via upload

--- download.dto.ts
+++ download.dto.ts
@@ -1,14 +1,7 @@
-// src/repository/interfaces/download.dto.ts
+export interface DownloadLinkDTO {
+  link: string;
+}
 
-// DTO for generating a download link
-export interface GenerateDownloadLinkDTO {
-    filename: string;
-    protocol: string;
-    host: string;
-  }
-  
-  // DTO for serving file by token
-  export interface ServeFileDTO {
-    token: string;
-  }
-  
+export interface FilePathDTO {
+  path: string;
+}
