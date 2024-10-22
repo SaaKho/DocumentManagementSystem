1b1804b7e215d90ae3ed4beb13af4dcca5b59265
Malik Saadan Khokhar <109228868+SaaKho@users.noreply.github.com>
2024-10-22 14:08:47 +0500

    Add files via upload

--- document.dto.ts
+++ document.dto.ts
@@ -1,4 +1,8 @@
 // src/application/dtos/DocumentDTO.ts
+import { z } from "zod";
+import { BaseDto } from "@carbonteq/hexapp";
+import { DocumentData } from "../../domain/entities/Document";
+
 export interface DocumentDTO {
   id: string;
   fileName: string;
@@ -10,13 +14,13 @@ export interface DocumentDTO {
   filePath: string;
 }
 
-export interface CreateDocumentDTO {
-  userId: string;
-  fileName: string;
-  fileExtension: string;
-  contentType: string;
-  tags?: string[];
-}
+// export interface CreateDocumentDTO {
+//   userId: string;
+//   fileName: string;
+//   fileExtension: string;
+//   contentType: string;
+//   tags?: string[];
+// }
 
 export interface UpdateDocumentDTO {
   fileName?: string;
@@ -24,4 +28,34 @@ export interface UpdateDocumentDTO {
   contentType?: string;
   tags?: string[];
   filePath?: string;
+}
+
+export interface UploadDocumentDTO {
+  fileName: string;
+  fileExtension: string;
+  contentType: string;
+  tags: string[];
+  userId: string;
+  documentId: string;
+  filePath: string;
+}
+
+export class DocumentCreateDTO extends BaseDto {
+  private static readonly schema = z.object({
+    userId: z.string(),
+    fileName: z.string(),
+    fileExtension: z.string(),
+    contentType: z.string(),
+    tags: z.array(z.string()),
+    filePath: z.string(),
+  });
+
+  private constructor(readonly data: Readonly<DocumentData>) {
+    super();
+  }
+  static create(data: unknown) {
+    return BaseDto.validate(DocumentCreateDTO.schema, data).map(
+      (parsed) => new DocumentCreateDTO(parsed)
+    );
+  }
 }
